8537f25c1bd42244da1d32bc43e7907f973bd5fd
LucasBorges21 <borgescorado@gmail.com>
2025-08-09 11:46:35 -0300

    Mudando tipo de preço para DECIMAL

--- app/models.py
+++ app/models.py
@@ -6,5 +6,5 @@ class Livro(db.Model):
     autor= db.Column(db.String(50), nullable=False)
     categoria= db.Column(db.String(30), nullable=True)
     editora= db.Column(db.String(40), nullable=True)
-    preco= db.Column(db.Integer, nullable=False)
+    preco= db.Column(db.Numeric(10, 2), nullable=False)
     ano_publicacao= db.Column(db.String(4), nullable=True)
