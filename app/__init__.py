38b6df1abdd2664296c93c362cc5ffc21f67bb09
LucasBorges21 <borgescorado@gmail.com>
2025-08-09 11:40:48 -0300

    Subindo BD e conexão com MySQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Gandalf1234r@localhost/flask_livro'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'SaoPaulo123' #alterar depois
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
 from app.routes import homepage
+from app.models import Livro
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Livro(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    titulo= db.Column(db.String(80), nullable=False)
+    autor= db.Column(db.String(50), nullable=False)
+    categoria= db.Column(db.String(30), nullable=True)
+    editora= db.Column(db.String(40), nullable=True)
+    preco= db.Column(db.Integer, nullable=False)
+    ano_publicacao= db.Column(db.String(4), nullable=True)
