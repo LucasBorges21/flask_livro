a4817db7e25d830cb72dbf4415ed776c82c08d85
LucasBorges21 <borgescorado@gmail.com>
2025-08-09 12:01:09 -0300

    Form de Cadastro HTML e pequenas alterações em routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
 from flask import render_template, redirect, url_for
+from app.models import Livro
+from app.forms import LivroForm
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro_livros():
+    form = LivroForm()
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('homepage'))
+    
+    return render_template('cadastro_livro.html', form=form)
